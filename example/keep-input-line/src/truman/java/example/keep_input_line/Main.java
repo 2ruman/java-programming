4f25d036bd3cdb6bb99d17075f2da8f0f9b57466
Truman <truman.t.kim@gmail.com>
2022-07-27 05:35:57 +0900

    - Switch main method with an alternative.

--- example/keep-input-line/src/truman/java/example/keep_input_line/Main.java
+++ example/keep-input-line/src/truman/java/example/keep_input_line/Main.java
@@ -4,47 +4,45 @@ import java.util.Scanner;
 
 public class Main {
 
-//    @SuppressWarnings("resource")
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        String input = "";
-//
-//        while (true) {
-//            // Display last input; It's empty at first
-//            System.out.println("Last Input : " + input);
-//
-//            // Get user input
-//            input = scanner.nextLine();
-//
-//            // Remove a single upper line using escape characters
-//            System.out.print("\033[1A\033[2K");
-//
-//            // Remove a single upper line again; 2 lines removed at last
-//            System.out.print("\033[1A\033[2K");
-//        }
-//    }
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        while (true) {
+            // Display last input; It's empty at first
+            System.out.println("Last Input : " + input);
+
+            // Get user input
+            input = scanner.nextLine();
+
+            // Remove a single upper line using escape characters
+            System.out.print("\033[1A\033[2K");
+
+            // Remove a single upper line again; 2 lines removed at last
+            System.out.print("\033[1A\033[2K");
+        }
+    }
 
     /**
      * The main method below will work the same as above as long as be launched
      * on a terminal, but not on virtual console like the one in eclipse.
      */
-    public static void main(String[] args) {
-        String input = "";
-
-        while (true) {
-            // Display last input; It's empty at first
-            System.console().printf("Last Input : %s\n", input);
-
-            // Get user input
-            input = System.console().readLine();
-
-            // Remove a single upper line using escape characters
-            System.console().printf("\033[1A\033[2K");
-
-            // Remove a single upper line again; 2 lines removed at last
-            System.console().printf("\033[1A\033[2K");
-        }
-    }
-
+//    public static void main(String[] args) {
+//        String input = "";
+//
+//        while (true) {
+//            // Display last input; It's empty at first
+//            System.console().printf("Last Input : %s\n", input);
+//
+//            // Get user input
+//            input = System.console().readLine();
+//
+//            // Remove a single upper line using escape characters
+//            System.console().printf("\033[1A\033[2K");
+//
+//            // Remove a single upper line again; 2 lines removed at last
+//            System.console().printf("\033[1A\033[2K");
+//        }
+//    }
 }
-
