8436f3974c71d1c3641d4a055c35914cf16813f8
Truman <truman.t.kim@gmail.com>
2024-09-24 20:12:05 +0900

    refactor: Change Command interface to be an abstract class which implements Callable interface so that it can be running on an executor service for itself

--- util/command-chain/src/main/java/truman/java/util/command_chain/Command.java
+++ util/command-chain/src/main/java/truman/java/util/command_chain/Command.java
@@ -1,6 +1,18 @@
 package truman.java.util.command_chain;
 
-public interface Command {
-    boolean execute();
-    void rollback();
+import java.util.concurrent.Callable;
+
+public abstract class Command implements Callable<Boolean> {
+    public abstract boolean execute();
+    public void rollback() {
+    }
+
+    @Override
+    public Boolean call() {
+        boolean result = execute();
+        if (!result) {
+            rollback();
+        }
+        return result;
+    }
 }
