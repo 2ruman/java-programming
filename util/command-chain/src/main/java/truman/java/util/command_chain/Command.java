4e819e44d8abf931cee406474905d297c82683cb
Truman <truman.t.kim@gmail.com>
2024-10-25 00:39:39 +0900

    feat: Restore Command class back to be interface

--- util/command-chain/src/main/java/truman/java/util/command_chain/Command.java
+++ util/command-chain/src/main/java/truman/java/util/command_chain/Command.java
@@ -1,18 +1,6 @@
 package truman.java.util.command_chain;
 
-import java.util.concurrent.Callable;
-
-public abstract class Command implements Callable<Boolean> {
-    public abstract boolean execute();
-    public void rollback() {
-    }
-
-    @Override
-    public Boolean call() {
-        boolean result = execute();
-        if (!result) {
-            rollback();
-        }
-        return result;
-    }
+public interface Command {
+    boolean execute();
+    void rollback();
 }
