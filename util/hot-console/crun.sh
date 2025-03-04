dc3aa9421bbb48d661106ae6e18444bf8676a294
Truman <truman.t.kim@gmail.com>
2025-03-05 01:04:45 +0900

    refactor: Modify the compile-and-run script to use relative paths

--- util/hot-console/crun.sh
+++ util/hot-console/crun.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Clean up all the class files
-rm -rf /truman/github-2ruman/java-programming/util/hot-console/bin
+rm -rf bin
 
 #Compile the main java with all dependencies
-javac -d /truman/github-2ruman/java-programming/util/hot-console/bin -sourcepath /truman/github-2ruman/java-programming/util/hot-console/src /truman/github-2ruman/java-programming/util/hot-console/src/truman/java/util/hot_console/Main.java
+javac -d bin -sourcepath src src/truman/java/util/hot_console/Main.java
 
 #Run the compiled main class
-java -cp /truman/github-2ruman/java-programming/util/hot-console/bin truman.java.util.hot_console.Main
+java -cp bin truman.java.util.hot_console.Main
 
