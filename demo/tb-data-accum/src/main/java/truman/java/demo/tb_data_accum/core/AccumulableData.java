896b55a05e552ac8a34d0f9314abb6c18c236ed0
Truman <truman.t.kim@gmail.com>
2025-11-10 20:29:05 +0900

    demo/tb-data-accum: Add type field to AccumulableData class

--- demo/tb-data-accum/src/main/java/truman/java/demo/tb_data_accum/core/AccumulableData.java
+++ demo/tb-data-accum/src/main/java/truman/java/demo/tb_data_accum/core/AccumulableData.java
@@ -1,13 +1,24 @@
 package truman.java.demo.tb_data_accum.core;
 
 public abstract class AccumulableData implements Accumulable {
+
+    private final int type;
     private final int uuid;
     private final long timestamp;
     private volatile Object tag;
 
     protected AccumulableData(int uuid, long timestamp) {
+        this(0, uuid, timestamp);
+    }
+
+    protected AccumulableData(int type, int uuid, long timestamp) {
+        this.type = type;
         this.uuid = uuid;
         this.timestamp = timestamp;
+    }
+
+    public int getType() {
+        return type;
     }
 
     public int getUuid() {
